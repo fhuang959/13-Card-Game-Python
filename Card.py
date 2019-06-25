e33c78da84ff583e7f4699568f020a89d413885f
Saumil <saumilkt@gmail.com>
2019-06-25 17:16:17 +0000

    Added Card.py

---
+++ Card.py
@@ -0,0 +1,9 @@
+class Card:
+    int rank
+    int suit
+    def __init__():
+        rank =0
+        suit =0
+    def __init__(int rank, int suit):
+        self.rank=rank
+        self.suit=suit
--- Deck.py
+++ Deck.py
@@ -1,5 +1,6 @@
-class hand:
+class Deck:
    static List
     def __init__():
        List = []
-    
+    def generate:
+
